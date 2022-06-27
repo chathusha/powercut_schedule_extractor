a255370fb784bef0999d246e215c5f10adb40e9f
Chathusha Karunathilake <mchathusha@gmail.com>
2022-06-27 23:07:03 +0530

    Get the date label
    - Get the date label from the web site.
    - Convert the label to datetime object.

--- src/extractor.py
+++ src/extractor.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from datetime import datetime
+
 
 def extract_data(url: str, path: str, group_name: str):
     """
@@ -19,8 +21,15 @@ def extract_data(url: str, path: str, group_name: str):
         (By.CSS_SELECTOR, "button[aria-label='next']")))
     next_day_btn.click()
 
-    time_data = driver.find_elements(By.CLASS_NAME, "fc-content")
-    # TODO: find the time relevent to the group
+    date_lbl = driver.find_element(By.CSS_SELECTOR, "h2").text
+    date = datetime.strptime(date_lbl, "%B %d, %Y").date()
+
+    time_data = [data.text for data in driver.find_elements(By.CLASS_NAME, "fc-content")]
+    
+    # TODO: extract start times and end times of each power cut
+
     # TODO: return the time data as a dictionary
 
     driver.quit()
+
+    return {"date": date}
