de411eac61072d274cd08d7554b3d8a26e55c38f
Chathusha Karunathilake <mchathusha@gmail.com>
2022-06-28 22:03:43 +0530

    Update the main.py file to take the updated method

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ url = "https://cebcare.ceb.lk/Incognito/DemandMgmtSchedule"
 path = "C:/Users/ROG/Documents/04. Hobbies/01. Python/07. Automation/powercut_schedule_extractor/src/chromedriver.exe"
 
 if __name__ == "__main__":
-    extractor.extract_data(url=url, path=path, group_name="K")
+    schedules = extractor.get_tomorrow_schedule(url, path, "K")
