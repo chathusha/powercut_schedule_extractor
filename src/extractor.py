3286ffbd6004a7e7de20ac83bbf5bcb4e901d6c1
Chathusha Karunathilake <mchathusha@gmail.com>
2022-06-28 21:55:03 +0530

    Return the start and end times of power cut schedules
    - Change the name of extractor method to get_tommorow_schedule.
    - Extract the start and end times of power cut schedules.
    - Convert the start and end times to datetime objects.
    - Update the date of datetime objects with tomorrow.
    - update the variable names and provide comments for more readability.

--- src/extractor.py
+++ src/extractor.py
@@ -6,17 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 
 
-def extract_data(url: str, path: str, group_name: str):
+def get_tomorrow_schedule(url: str, path: str, group_name: str) -> dict:
     """
     Extracts demand shedding data relevent to tommorow from the given url.
     :param url: url to extract data from
     :param path: path to webdriver
     :param group_name: name of the group in capital letters
-    :return: data extracted from the url
+    :return: start times and end times of power cut shedule
     """
     driver = webdriver.Chrome(path)
     driver.get(url)
 
+    # load the schedule for tommorow
     next_day_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, "button[aria-label='next']")))
     next_day_btn.click()
@@ -24,12 +25,18 @@ def extract_data(url: str, path: str, group_name: str):
     date_lbl = driver.find_element(By.CSS_SELECTOR, "h2").text
     date = datetime.strptime(date_lbl, "%B %d, %Y").date()
 
-    time_data = [data.text for data in driver.find_elements(By.CLASS_NAME, "fc-content")]
+    # extract powercut schedules from the CEB website
+    schedules = [data.text for data in WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "fc-content")))]
     
-    # TODO: extract start times and end times of each power cut
+    # extract start time and endtime of power cut schedules
+    start_times = [datetime.strptime(schedule.split()[0], "%I:%M%p") for schedule in schedules if group_name in schedule]
+    end_times = [datetime.strptime(schedule.split()[1], "%I:%M%p") for schedule in schedules if group_name in schedule]
 
-    # TODO: return the time data as a dictionary
+    # replace date of start_times and end_times with extracted date
+    start_times = [time.replace(year=date.year, month=date.month, day=date.day) for time in start_times]
+    end_times = [time.replace(year=date.year, month=date.month, day=date.day) for time in end_times]
 
     driver.quit()
 
-    return {"date": date}
+    return {"start_times": start_times, "end_times": end_times}
